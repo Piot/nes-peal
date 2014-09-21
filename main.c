2ec167e5c298be30499f39c63aefcfac86b16358
Peter Bjorklund <piot@hotmail.com>
2014-09-21 15:49:16 +0200

    Cleaned up code and added a game loop.

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 typedef unsigned char u8;
 
+u8 player1_x = 0;
+u8 player1_y = 2;
+
+u8 temp;
+u8 frame_counter = 0;
+
 #define ppu_background_scrolling_offset(a, b) *((u8*) 0x2005) = a; *((u8*) 0x2005) = b;
 
 #define vram_address_register(a, b) *((u8*) 0x2006) = a; *((u8*) 0x2006) = b;
@@ -10,9 +16,12 @@ typedef unsigned char u8;
 
 #define ppu_control_register_2(a) *((u8*) 0x2001) = a;
 
+#define set_name_position_to_vram_with_offset(o, x, y) vram_address_register(o + (y >> 3), ((y << 5) + x) & 0xff); 
+
+#define set_name_position_to_vram(x, y)  set_name_position_to_vram_with_offset(0x20, x, y);
+
 void write_string(const char* s)
 {
-	vram_address_register(0x20, 0x41);
 	while (*s)
 	{
 		vram_write(*s);
@@ -20,15 +29,33 @@ void write_string(const char* s)
 	}
 }
 
+
+#define set_char(x, y, c) set_name_position_to_vram(x, y); vram_write(c); 
+
 void enable_screen()
 {
 	ppu_control_register_2(0x08);
 }
 
-void wait_forever()
+void disable_screen()
 {
-	while (1)
-		;
+	ppu_control_register_2(0x00);
+}
+
+
+void game_loop()
+{
+	frame_counter++;
+	if (frame_counter % 8 != 0) {
+		return;
+	}
+	if (player1_x == 30)
+	{
+		return;
+	}
+	set_char(player1_x, player1_y, ' ');
+	player1_x++;
+	set_char(player1_x, player1_y, 'x');
 }
 
 void setup_colors()
@@ -48,16 +75,19 @@ void zero_screen_position()
 int main()
 {
 	waitvblank();
-
+	disable_screen();
 	setup_colors();
 
-	write_string("André is cool!");
+	set_name_position_to_vram(2, 3);
+	write_string("Andre is cool!");
 
-	zero_screen_position();	
-	
-	enable_screen();
-
-	wait_forever();
+	while (1) {
+		waitvblank();
+		disable_screen();
+		game_loop();
+		zero_screen_position();
+		enable_screen();
+	}
 
 	return 0;
 }
