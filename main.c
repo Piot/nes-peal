1ec828b5d3175fadc4b6b6a7209c572a9d65b4f0
Peter Bjorklund <piot@hotmail.com>
2014-09-21 16:19:57 +0200

    added basic joypad input and move

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 
 typedef unsigned char u8;
 
-u8 player1_x = 0;
+u8 player1_x = 1;
 u8 player1_y = 2;
+
+u8 joypad1_data = 0;
+u8 joypad2_data = 0;
 
 u8 temp;
 u8 frame_counter = 0;
@@ -16,9 +19,14 @@ u8 frame_counter = 0;
 
 #define ppu_control_register_2(a) *((u8*) 0x2001) = a;
 
-#define set_name_position_to_vram_with_offset(o, x, y) vram_address_register(o + (y >> 3), ((y << 5) + x) & 0xff); 
+#define set_name_position_to_vram_with_offset(o, x, y) vram_address_register(o + (y >> 3), (((y << 5) & 0xff)+ x) & 0xff); 
 
 #define set_name_position_to_vram(x, y)  set_name_position_to_vram_with_offset(0x20, x, y);
+
+#define joypad_strobe *((u8 *) 0x4016) = 0x01; *((u8 *) 0x4016) = 0x00;
+
+#define joypad1_read (*((u8*) 0x4016) & 0x01)
+#define joypad2_read (*((u8*) 0x4017) & 0x01)
 
 void write_string(const char* s)
 {
@@ -30,7 +38,20 @@ void write_string(const char* s)
 }
 
 
-#define set_char(x, y, c) set_name_position_to_vram(x, y); vram_write(c); 
+#define set_char(x, y, c) set_name_position_to_vram(x, y); vram_write(c);
+
+void read_joypads()
+{
+	u8 i;
+
+	joypad_strobe;
+	joypad1_data = 0;
+
+	for (i=0; i<8; ++i) {
+		joypad1_data <<= 1;
+		joypad1_data |= joypad1_read;
+	}
+}
 
 void enable_screen()
 {
@@ -45,16 +66,29 @@ void disable_screen()
 
 void game_loop()
 {
+	read_joypads();
 	frame_counter++;
-	if (frame_counter % 8 != 0) {
-		return;
-	}
-	if (player1_x == 30)
-	{
+	if (frame_counter % 4 != 0) {
 		return;
 	}
 	set_char(player1_x, player1_y, ' ');
-	player1_x++;
+	if (joypad1_data & 0x01) {
+		if (player1_x < 31) {
+			player1_x++;
+		}
+	} else if (joypad1_data & 0x02) {
+		if (player1_x > 1) {
+			player1_x--;
+		}
+	} else if (joypad1_data & 0x04) {
+		if (player1_y < 29) {
+			player1_y++;
+		}
+	} else if (joypad1_data & 0x08) {
+		if (player1_y > 0) {
+			player1_y--;
+		}
+	}
 	set_char(player1_x, player1_y, 'x');
 }
 
@@ -75,18 +109,18 @@ void zero_screen_position()
 int main()
 {
 	waitvblank();
-	disable_screen();
 	setup_colors();
 
 	set_name_position_to_vram(2, 3);
 	write_string("Andre is cool!");
 
+	enable_screen();
 	while (1) {
 		waitvblank();
 		disable_screen();
 		game_loop();
-		zero_screen_position();
 		enable_screen();
+		zero_screen_position();
 	}
 
 	return 0;
